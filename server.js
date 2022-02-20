06c12bea0c553c9ef2768f93579d0b91dae30229
Rafael Fonseca <fonseca.wutang@gmail.com>
2022-02-20 07:56:15 +0000

    add route js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index")
+})
+
+app.listen(8082);
+console.log("rodando");
